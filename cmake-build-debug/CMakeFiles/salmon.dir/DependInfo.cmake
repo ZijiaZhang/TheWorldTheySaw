1f5585d8d35c3345858a2c939087407de5004f96
Gary <zijia.zhang99@gmail.com>
2021-02-01 22:43:17 -0800

    Add template

---
+++ cmake-build-debug/CMakeFiles/salmon.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Gary/CLionProjects/Team12/src/ai.cpp" "C:/Users/Gary/CLionProjects/Team12/cmake-build-debug/CMakeFiles/salmon.dir/src/ai.cpp.obj"
+  "C:/Users/Gary/CLionProjects/Team12/src/common.cpp" "C:/Users/Gary/CLionProjects/Team12/cmake-build-debug/CMakeFiles/salmon.dir/src/common.cpp.obj"
+  "C:/Users/Gary/CLionProjects/Team12/src/debug.cpp" "C:/Users/Gary/CLionProjects/Team12/cmake-build-debug/CMakeFiles/salmon.dir/src/debug.cpp.obj"
+  "C:/Users/Gary/CLionProjects/Team12/src/fish.cpp" "C:/Users/Gary/CLionProjects/Team12/cmake-build-debug/CMakeFiles/salmon.dir/src/fish.cpp.obj"
+  "C:/Users/Gary/CLionProjects/Team12/src/main.cpp" "C:/Users/Gary/CLionProjects/Team12/cmake-build-debug/CMakeFiles/salmon.dir/src/main.cpp.obj"
+  "C:/Users/Gary/CLionProjects/Team12/src/pebbles.cpp" "C:/Users/Gary/CLionProjects/Team12/cmake-build-debug/CMakeFiles/salmon.dir/src/pebbles.cpp.obj"
+  "C:/Users/Gary/CLionProjects/Team12/src/physics.cpp" "C:/Users/Gary/CLionProjects/Team12/cmake-build-debug/CMakeFiles/salmon.dir/src/physics.cpp.obj"
+  "C:/Users/Gary/CLionProjects/Team12/src/render.cpp" "C:/Users/Gary/CLionProjects/Team12/cmake-build-debug/CMakeFiles/salmon.dir/src/render.cpp.obj"
+  "C:/Users/Gary/CLionProjects/Team12/src/render_components.cpp" "C:/Users/Gary/CLionProjects/Team12/cmake-build-debug/CMakeFiles/salmon.dir/src/render_components.cpp.obj"
+  "C:/Users/Gary/CLionProjects/Team12/src/render_init.cpp" "C:/Users/Gary/CLionProjects/Team12/cmake-build-debug/CMakeFiles/salmon.dir/src/render_init.cpp.obj"
+  "C:/Users/Gary/CLionProjects/Team12/src/salmon.cpp" "C:/Users/Gary/CLionProjects/Team12/cmake-build-debug/CMakeFiles/salmon.dir/src/salmon.cpp.obj"
+  "C:/Users/Gary/CLionProjects/Team12/src/tiny_ecs.cpp" "C:/Users/Gary/CLionProjects/Team12/cmake-build-debug/CMakeFiles/salmon.dir/src/tiny_ecs.cpp.obj"
+  "C:/Users/Gary/CLionProjects/Team12/src/turtle.cpp" "C:/Users/Gary/CLionProjects/Team12/cmake-build-debug/CMakeFiles/salmon.dir/src/turtle.cpp.obj"
+  "C:/Users/Gary/CLionProjects/Team12/src/world.cpp" "C:/Users/Gary/CLionProjects/Team12/cmake-build-debug/CMakeFiles/salmon.dir/src/world.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "../ext/stb_image"
+  "../ext/gl3w"
+  "../ext/glfw/include"
+  "../ext/sdl/include/SDL"
+  "../ext/glm"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/salmon.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/salmon.dir/src/ai.cpp.obj"
+  "CMakeFiles/salmon.dir/src/common.cpp.obj"
+  "CMakeFiles/salmon.dir/src/debug.cpp.obj"
+  "CMakeFiles/salmon.dir/src/fish.cpp.obj"
+  "CMakeFiles/salmon.dir/src/main.cpp.obj"
+  "CMakeFiles/salmon.dir/src/pebbles.cpp.obj"
+  "CMakeFiles/salmon.dir/src/physics.cpp.obj"
+  "CMakeFiles/salmon.dir/src/render.cpp.obj"
+  "CMakeFiles/salmon.dir/src/render_components.cpp.obj"
+  "CMakeFiles/salmon.dir/src/render_init.cpp.obj"
+  "CMakeFiles/salmon.dir/src/salmon.cpp.obj"
+  "CMakeFiles/salmon.dir/src/tiny_ecs.cpp.obj"
+  "CMakeFiles/salmon.dir/src/turtle.cpp.obj"
+  "CMakeFiles/salmon.dir/src/world.cpp.obj"
+  "salmon.exe"
+  "salmon.exe.manifest"
+  "salmon.lib"
+  "salmon.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/salmon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
