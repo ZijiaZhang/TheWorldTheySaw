7186f19308648058661fabed414f687e50c52f83
jieluo <jieluo122@gmail.com>
2021-03-19 15:44:13 -0700

    add comment and remove the circle range

--- data/shaders/shinning.fs.glsl
+++ data/shaders/shinning.fs.glsl
@@ -18,40 +18,41 @@ vec4 anisotropic(vec3 vColor) {
     float specular = 0.5;
     float amb = 0.4;
     vec3 ambColor = vec3(0.4, 0.4, 0.4);
+
     vec3 vPosition = vec3(ori_pos - vpos, 0);
     vec3 uLight = vec3(0.49,0.79, 0.49);
+    // the normal of the point,
+    // since it's 2d, we shift by the position on normal to make it visually varies
     vec3 vNormal = vec3(0, 0, 1) - vPosition;
-    vec3 tangent = vec3(0, 0.5, 0);
     vec3 toLight = normalize(uLight);
-    vec3 toV = -normalize(vPosition);
-    vec3 h = normalize(toV + toLight);
-    vec3 normal = normalize(vNormal);
-    vec3 vTangent3 = normalize(cross(vNormal, tangent));
 
+    // diffuse, if nl is large means the angle between normal and toLight is smaller
+    vec3 normal = normalize(vNormal);
     float nl = dot(normal, toLight);
     float dif = max(0.0, 0.75 * nl + 0.25);
+
+    // the far from center reflect, the less shininess
+    vec3 toV = -normalize(vPosition);// the view vector,
+    vec3 h = normalize(toV + toLight);  // half vector
+    vec3 tangent = vec3(0.5, .5, 0);
+    vec3 vTangent3 = normalize(cross(vNormal, tangent));
     float v = dot(vTangent3, h);
     v = pow(1.0 - v*v, shininess);
-    float r = vColor.r * (dif ) + specular * v + ambColor.x * amb;
-    float g = vColor.g * (dif) + specular * v + ambColor.y * amb;
-    float b = vColor.b * (dif ) + specular * v + ambColor.z * amb;
 
-    return vec4(r, g, b, 1.0);
+    // diffuse + specular + ambient
+    vec3 c = vColor * dif + specular * v + ambColor * amb;
+
+    return vec4(c, 1.0);
 }
 
 void main()
 {
     color = vec4(fcolor, 1.0) * texture(sampler0, vec2(texcoord.x, texcoord.y));
-    float range = 0.4;
+
     if(color.a < 0.8) {
         discard;
     }
-    float distance = distance(vec2(0, 0), vpos);
 
-    if (distance <= range) {
-        color = anisotropic(color.xyz);
-    } else {
-        color = vec4(0.1, 0.1, 0.1, 1);
-    }
+    color = anisotropic(color.xyz);
 
 }
