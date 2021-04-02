0c2837e44705095e854322132a4847d1bfd8862a
Gary <gary1999@students.cs.ubc.ca>
2021-04-01 18:00:59 -0700

    Fix some stuff

--- data/shaders/lights.fs.glsl
+++ data/shaders/lights.fs.glsl
@@ -11,31 +11,27 @@ in vec2 texcoord;
 in vec2 world_pos;
 
 const float max_step = 600.0;
-const float accuracy = 255;
-const float pi = radians(180);
+const float accuracy = 255.0;
+const float pi = radians(180.0);
 layout(location = 0) out vec4 color;
 
 void main()
 {
 	vec2 coord = floor(texcoord * texture_size);
 	// float world_deg = world_size.y/ world_size.x;
-    float ray_count = texture_size * texture_size;
+	float ray_count = texture_size * texture_size;
 	float ray_of_current_pixel = coord.x + coord.y * texture_size;
 	float radian = 2.0 * pi * ray_of_current_pixel/ray_count;
 	vec2 angle = normalize(vec2(cos(radian), sin(radian)));
 
-	float i = 1;
-	for(i=0; i< max_step; i+=3){
-		if(texture2D(screen_texture, (player_position * world_size + angle * i) / world_size).a > 0.5) break;
+	float t = 0.0;
+	for(float i = 0.0; i< max_step; i+=3.0){
+		if(texture2D(screen_texture, (player_position * world_size + angle * i) / world_size).a > 0.5) {
+			t = i;
+			break;
+		};
 	}
-	i--;
-
-	float d2 = floor(i/ accuracy)/accuracy;
-	float d1 = mod(i, accuracy) / accuracy;
-	color = vec4(d2, d1,0.0,1.0);
-	if(length(angle) < 0.9){
-		color = vec4(1.0, 1.0, 1.0, 1.0);
-	}
+	gl_FragColor = vec4(floor(t/ accuracy)/accuracy, mod(t, accuracy) / accuracy,0.0,1.0);
 //	color = vec4(cos(radian), sin(radian), 0.0, 1.0);
 
 
